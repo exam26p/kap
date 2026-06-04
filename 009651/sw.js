d0b65e916c0a8e2a5dab8ad2c2e98a2aede8d7ce
exam26p <hareerwissam@gmail.com>
2026-06-04 17:27:07 +0300

    Update sw.js

--- 009651/sw.js
+++ 009651/sw.js
@@ -1,39 +1,26 @@
 const CACHE_NAME = 'captain-app-v1';
 const urlsToCache = [
-  '/index2.html',
-  '/captain.js',
-  '/icone.ico'
+  './',
+  './index2.html',
+  './captain.js',
+  './icone.ico'
 ];
-self.addEventListener('install', function(event) {
+
+self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(function(cache) {
+      .then(cache => {
         console.log('Opened cache');
         return cache.addAll(urlsToCache);
       })
   );
 });
-self.addEventListener('fetch', function(event) {
+
+self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(function(response) {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
+      .then(response => {
+        return response || fetch(event.request);
       })
-  );
-});
-self.addEventListener('activate', function(event) {
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.map(function(cacheName) {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
   );
 });
